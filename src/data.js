d7ce499ece80cecfeb486a2fceb47ad4c2f36d11
claudgomezg <claudgomez@gmail.com>
2021-09-28 17:51:22 -0300

    Test ordenar card default

--- src/data.js
+++ src/data.js
@@ -1,9 +1,14 @@
-// estas funciones son de ejemplo
-
-export const example = () => {
-  return 'example';
+const orderData = {
+  orderDefault: function (printFilm) {
+    let orderedResults = printFilm.sort(function (a, b) {
+    if (a.title > b.title) {
+      return 1; // retornar valores a la derecha (coordenadas)
+    } else {
+      return -1; // retornar valores a la izquierda (coordenadas)
+    }
+  });
+  return orderedResults;
+},
 };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+export default orderData;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,28 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/ghibli/ghibli.js'
+import orderData from './data.js'
 
-console.log(example, data);
+let printFilm = data.films
+
+const movieList = function (films) {
+  let list = ''
+  document.getElementById('filmCard').innerHTML = ''
+
+  for (let i = 0; i < films.length; i++) {
+    list += `
+    <div class= "cardContainer">
+    <div id="printTitle" class="cardContainer-inner">
+        <div class="frontCard">
+        <img id="poster" class="poster" src="${films[i].poster}"/>
+        <div class="nametag">
+        <p id="title" class="title">${films[i].title}</p>
+        <p id="release_date" class="release_date">Release date: ${films[i].release_date}</p>
+        <p id="director" class="director">Director: ${films[i].director}</p>
+          </div>
+        </div>
+      </div>
+    </div>`
+  }
+  document.getElementById('filmCard').innerHTML = list
+}
+movieList(printFilm)
+console.log(orderData, data)
